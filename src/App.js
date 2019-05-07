06a0405e3fe0a7181f47d56b6d1cd1cebe81ee4b
Deki <dekiakbar@linuxmail.org>
2019-05-07 22:25:47 +0700

    layouting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import 'bootstrap/scss/bootstrap.scss';
+import './assets/scss/app.scss';
+import Homestays from './components/Homestays';
 function App() {
   return (
     <div className="App">
-      
+      <div className="wrapper row">
+        <div className="main col-6">
+          <div className="homestays">
+            <Homestays/>
+          </div>
+        </div>
+
+        <div className="map col-6">
+        
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Homestays.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Homestays extends Component{
+
+    render(){
+        return(
+            <p>haiiiii</p>
+        )
+    }
+}
+
+export default Homestays;
