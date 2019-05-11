08913d9f240d2dfc088c6bda139f0552457bba04
Deki <dekiakbar@linuxmail.org>
2019-05-11 16:46:53 +0700

    fetch data from json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React,{Component} from 'react';
 import 'bootstrap/scss/bootstrap.scss';
 import './assets/scss/app.scss';
 import Homestays from './components/Homestays';
-function App() {
-  return (
-    <div className="App">
-      <div className="wrapper row">
-        <div className="main col-6">
-          <div className="homestays">
-            <Homestays/>
+class App extends Component{
+
+  constructor (props){
+    super (props);
+    this.state={
+      homestays : []
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://gist.githubusercontent.com/dekiakbar/f31eff6542920ceea46db42df1e6513f/raw/06b0c3947dcc7820f1d956bfc3166a7df35f7dd9/homestay.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          homestays:data
+        })
+      })
+  }
+
+  render(){
+    const homestay={
+      "nama"  : "Hotel Maju Mundur",
+      "fotoUrl" : "https://drive.google.com/uc?id=1Z16fbOIpI9H_JglsSJ1V8MNG6UloURlM",
+      "harga" : 900
+    }
+    return (
+      <div className="App">
+        <div className="wrapper row">
+
+          <div className="main col-7">
+            <div className="homestays row">
+              
+              {this.state.homestays.map((homestay) => {
+                return <Homestays key={homestay.id} homestay={homestay} />
+              })}
+
+            </div>
+          </div>
+
+          <div className="map col-5">
+          
           </div>
         </div>
-
-        <div className="map col-6">
-        
-        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Homestays.js
+++ src/components/Homestays.js
@@ -3,8 +3,19 @@ import React,{Component} from 'react';
 class Homestays extends Component{
 
     render(){
+        const title = `${this.props.homestay.nama} - Rp. ${this.props.homestay.harga}K`
+        const img = this.props.homestay.fotoUrl;
         return(
-            <p>haiiiii</p>
+            <div className="col-6">
+                <div className="card homestay">
+                    <img className="card-img-top" src={img} alt={title} />
+                    <div className="card-body">
+                        <h6 className="card-title">
+                            {title}
+                        </h6>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
