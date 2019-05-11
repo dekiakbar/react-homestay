a88cfc51128166f1f3f1b596c9e6908be6f8f018
Deki <dekiakbar@linuxmail.org>
2019-05-11 19:03:17 +0700

    fetch JSON append to google maps react, map marker homestay

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React,{Component} from 'react';
 import 'bootstrap/scss/bootstrap.scss';
 import './assets/scss/app.scss';
 import Homestays from './components/Homestays';
+import GoogleMapReact from 'google-map-react';
+import Marker from './components/Marker';
+
 class App extends Component{
 
   constructor (props){
     super (props);
     this.state={
-      homestays : []
+      homestays : [],
+      selectedHomestay:null
     }
   }
 
   componentDidMount(){
-    fetch("https://gist.githubusercontent.com/dekiakbar/f31eff6542920ceea46db42df1e6513f/raw/06b0c3947dcc7820f1d956bfc3166a7df35f7dd9/homestay.json")
+    fetch("https://gist.githubusercontent.com/dekiakbar/f31eff6542920ceea46db42df1e6513f/raw/f2d6798cd8bb54d1064a38e85df3c82b93b713fa/homestay.json")
       .then(response => response.json())
       .then((data) => {
         this.setState({
@@ -21,28 +25,46 @@ class App extends Component{
       })
   }
 
+  selectHomestay = (homestay) => {
+    this.setState({
+      selectedHomestay : homestay
+    })
+  }
+
   render(){
-    const homestay={
-      "nama"  : "Hotel Maju Mundur",
-      "fotoUrl" : "https://drive.google.com/uc?id=1Z16fbOIpI9H_JglsSJ1V8MNG6UloURlM",
-      "harga" : 900
+    let center={
+      lat:-7.180564,
+      lng:106.4649628
+    }
+
+    if(this.state.selectedHomestay){
+      center={
+        lat:this.state.selectedHomestay.lat,
+        lng:this.state.selectedHomestay.lng
+      }
     }
     return (
       <div className="App">
         <div className="wrapper row">
 
-          <div className="main col-7">
+          <div className="main col-6">
             <div className="homestays row">
               
               {this.state.homestays.map((homestay) => {
-                return <Homestays key={homestay.id} homestay={homestay} />
+                return <Homestays key={homestay.id} homestay={homestay} selectHomestay={this.selectHomestay}/>
               })}
 
             </div>
           </div>
 
-          <div className="map col-5">
-          
+          <div className="map col-6">
+              <GoogleMapReact center={center} zoom={16}>
+                {
+                  this.state.homestays.map((homestay) => {
+                    return <Marker key={homestay.id} lat={homestay.lat} lng={homestay.lng} harga={homestay.harga} selected={homestay === this.state.selectedHomestay} />
+                  })
+                }
+              </GoogleMapReact>
           </div>
         </div>
       </div>
--- src/components/Homestays.js
+++ src/components/Homestays.js
@@ -1,16 +1,18 @@
 import React,{Component} from 'react';
 
 class Homestays extends Component{
-
+    handleClick= () =>{
+        this.props.selectHomestay(this.props.homestay);
+    }
     render(){
         const title = `${this.props.homestay.nama} - Rp. ${this.props.homestay.harga}K`
         const img = this.props.homestay.fotoUrl;
         return(
             <div className="col-6">
-                <div className="card homestay">
+                <div className="card homestay" onClick={this.handleClick}>
                     <img className="card-img-top" src={img} alt={title} />
                     <div className="card-body">
-                        <h6 className="card-title">
+                        <h6 className="card-text">
                             {title}
                         </h6>
                     </div>
